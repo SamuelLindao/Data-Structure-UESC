189c2ca235befc7929ae166272ed58a0a0535b91
Samuel Ferreira <75032911+SamuelLindao@users.noreply.github.com>
2025-05-14 15:34:00 -0300

    Add files via upload

---
+++ StackAndQueue/stack.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct no{
+  int data;
+  struct no*prox;
+  
+};
+typedef struct no tprNo;
+
+struct stack{
+    int n;
+    struct no* top;
+
+};
+typedef struct stack tprStack;
+
+void printStack(tprStack *stack)
+{
+    if(stack ->top == NULL) return;
+    tprNo *aux = stack->top;
+    while(aux != NULL)
+    {
+        printf(" |  aux == %i -- %i |", aux->data, stack->n);
+        aux = aux->prox;
+    }
+    printf("\n\n");
+}
+
+int insert(tprStack *stack, int data)
+{
+    if(stack == NULL) return 0;
+    tprNo *new = (tprNo*)malloc(sizeof(tprNo));
+    if(new == NULL) return 0 ;
+    new->prox = stack->top;
+    new->data = data;
+    
+    stack->top = new;
+    stack->n++;
+    return 1;
+}
+
+
+int main()
+{
+    tprStack *stack = (tprStack*)malloc(sizeof(tprStack));
+    stack->top = NULL;
+    stack->n = 0;
+    
+    insert(stack, 10);
+    insert(stack, 11);
+    insert(stack, 12);
+    insert(stack, 13);
+    printStack(stack);
+    return 0;
+}
