f5bf1a31c0d6e6a39ff7fc82eeb9fb981267c80b
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-21 21:22:23 -0300

    init()

--- SemanticTree/main.c
+++ SemanticTree/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Item{
     int valor;
@@ -8,7 +9,18 @@ struct NO {
     tprItem item;
     struct NO *esq, *dir;
 };
+
+typedef struct NO tpNo;
+
+tpNo *init() {
+    tpNo *no = (tpNo*)malloc(sizeof(tpNo));
+    no->esq = NULL;
+    no->dir = NULL;
+
+    return no;
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    tpNo *binary = init();
     return 0;
 }
